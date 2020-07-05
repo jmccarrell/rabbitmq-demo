d2385a08fd637e5607ab30e54e6120ff43a502d6
Jeff McCarrell <jeff@mccarrell.org>
2020-07-04 19:35:20 -0700

    keep going when the consumer times out.

--- hello_consumer.py
+++ hello_consumer.py
@@ -1,8 +1,12 @@
+import sys
 from kombu import Connection
 
 with Connection('amqp://user:bitnami@localhost:5672//') as conn:
     with conn.SimpleQueue('simple_queue') as simple_queue:
         while True:
-            msg = simple_queue.get(block=True, timeout=1)
-            print(f'Received: {msg.payload}')
-            msg.ack()
+            try:
+                msg = simple_queue.get(block=True, timeout=1)
+                print(f'Received: {msg.payload}')
+                msg.ack()
+            except Exception as exc:
+                print(f'caught exception: {exc}', file=sys.stderr)
