8d1d30eab98f7121b7c4a3155c31738a56f992a2
Jeff McCarrell <jeff@mccarrell.org>
2020-07-04 20:11:08 -0700

    move rabbitmq authentication details into the environment.

--- hello_consumer.py
+++ hello_consumer.py
@@ -1,7 +1,27 @@
 import sys
+from os import environ
 from kombu import Connection
 
-with Connection('amqp://user:bitnami@localhost:5672//') as conn:
+required_environment_vars = tuple(('RABBITMQ_USERNAME',
+                                   'RABBITMQ_PASSWORD',
+                                   'RABBITMQ_NODE_PORT_NUMBER',
+                                   'RABBITMQ_VHOST',))
+
+
+def make_connection_url() -> str:
+    # check for required runtime parameters
+    missing_env_vars = []
+    for e in required_environment_vars:
+        if environ.get(e) is None:
+            missing_env_vars.append(e)
+    if len(missing_env_vars) > 0:
+        raise RuntimeError(f'expected environment vars for config: {missing_env_vars}')
+
+    return ''.join((f"amqp://{environ['RABBITMQ_USERNAME']}:{environ['RABBITMQ_PASSWORD']}",
+                    f"@localhost:{environ['RABBITMQ_NODE_PORT_NUMBER']}/{environ['RABBITMQ_VHOST']}"))
+
+
+with Connection(make_connection_url()) as conn:
     with conn.SimpleQueue('simple_queue') as simple_queue:
         while True:
             try:
