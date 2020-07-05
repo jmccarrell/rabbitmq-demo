fe0246b5dd296d7bff0ef259c043e916382571cc
Jeff McCarrell <jeff@mccarrell.org>
2020-07-05 14:36:10 -0700

    implement producer and consumer rate limits
    using the leaky bucket implementation from kombu
    which came from a recipe from Active State

--- hello_consumer.py
+++ hello_consumer.py
@@ -1,16 +1,25 @@
 import sys
+import time
 
 from kombu import Connection
 from connection import make_connection_url
+from token_bucket import TokenBucket
 
+consume_rate = 6  # per second
+rate_limit = TokenBucket(consume_rate, consume_rate * 10)
 
 # print(f'connection string: {make_connection_url()}', file=sys.stderr)
 with Connection(make_connection_url()) as conn:
     with conn.SimpleQueue('simple_queue') as simple_queue:
         while True:
             try:
-                msg = simple_queue.get(block=True, timeout=1)
-                print(f'Received: {msg.payload}')
-                msg.ack()
+                if rate_limit.can_consume(1):
+                    msg = simple_queue.get(block=True, timeout=1)
+                    print(f'Received: {msg.payload}')
+                    msg.ack()
+                else:
+                    sleep_for = rate_limit.expected_time()
+                    print(f'consume_rate: {consume_rate}; sleep({sleep_for})')
+                    time.sleep(rate_limit.expected_time())
             except Exception as exc:
                 print(f'caught exception: {exc}', file=sys.stderr)
--- hello_publisher.py
+++ hello_publisher.py
@@ -1,13 +1,26 @@
 import sys
+import time
 import datetime
 
 from kombu import Connection
 from connection import make_connection_url
+from token_bucket import TokenBucket
 
+produce_rate = 5  # per second
+rate_limit = TokenBucket(produce_rate, produce_rate * 10)
 
 # print(f'connection string: {make_connection_url()}', file=sys.stderr)
 with Connection(make_connection_url()) as conn:
     with conn.SimpleQueue('simple_queue') as simple_queue:
-        msg = f'hello Jeff; sent at {datetime.datetime.now()}'
-        simple_queue.put(msg)
-        print(f'Sent: {msg}')
+        while True:
+            try:
+                if rate_limit.can_consume(1):
+                    msg = f'hello Jeff at rate {produce_rate}/second; sent at {datetime.datetime.now()}'
+                    simple_queue.put(msg)
+                    print(f'Sent: {msg}')
+                else:
+                    # sleep_for = rate_limit.expected_time()
+                    # print(f'sleep({sleep_for})')
+                    time.sleep(rate_limit.expected_time())
+            except Exception as exc:
+                print(f'caught exception: {exc}', file=sys.stderr)
